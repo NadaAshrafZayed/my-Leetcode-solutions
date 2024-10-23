dd1d4f9f1c3ade25027a7653857f8524987b43e5
NadaAshrafZayed <118112548+NadaAshrafZayed@users.noreply.github.com>
2024-10-23 06:06:37 +0300

    Time: 0 ms (100.00%) | Memory: 14.6 MB (14.45%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, vector<int>>mp;
-        vector<int>ans;
-        for(int i=0 ; i<nums.size() ; i++){
-            mp[nums[i]].push_back(i);
+        // The numbers and their indices
+        map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target - nums[i]) == mp.end())
+                // If not, add the current number and its index to the map
+                mp[nums[i]] = i;
+            else
+                // If yes, return the indices of the current number and its complement
+                return {mp[target - nums[i]], i};
         }
-
-        for(int i=0 ; i<nums.size() ; i++){
-            if( mp.find( target-nums[i] ) != mp.end() ){
-                if(nums[i] == target-nums[i]){
-                    if(mp[ target-nums[i] ].size() >=2){
-                        ans.push_back(i);
-                        ans.push_back( mp[ target-nums[i] ][1] );
-                        break;
-                    }
-                }else{
-                    ans.push_back(i);
-                    ans.push_back( mp[ target-nums[i] ][0] );
-                    break;
-                }
-            }
-        }
-        
-        return ans;
+        return {-1, -1};
     }
 };
