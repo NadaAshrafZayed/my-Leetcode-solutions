d697e3ef3f7be47c232ee2fb90e6cff3a60aeef7
NadaAshrafZayed <118112548+NadaAshrafZayed@users.noreply.github.com>
2024-10-23 06:08:19 +0300

    Time: 5 ms (87.50%) | Memory: 14.7 MB (14.45%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -13,4 +13,32 @@ public:
         }
         return {-1, -1};
     }
+    
+    
+//     My old solution (Above is better)
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         map<int, vector<int>>mp;
+//         vector<int>ans;
+//         for(int i=0 ; i<nums.size() ; i++){
+//             mp[nums[i]].push_back(i);
+//         }
+
+//         for(int i=0 ; i<nums.size() ; i++){
+//             if( mp.find( target-nums[i] ) != mp.end() ){
+//                 if(nums[i] == target-nums[i]){
+//                     if(mp[ target-nums[i] ].size() >=2){
+//                         ans.push_back(i);
+//                         ans.push_back( mp[ target-nums[i] ][1] );
+//                         break;
+//                     }
+//                 }else{
+//                     ans.push_back(i);
+//                     ans.push_back( mp[ target-nums[i] ][0] );
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+    
 };
