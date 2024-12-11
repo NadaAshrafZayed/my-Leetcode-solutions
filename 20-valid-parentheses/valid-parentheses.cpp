82e6d50db461ea656e9fd71502ec5b60600bad5d
NadaAshrafZayed <118112548+NadaAshrafZayed@users.noreply.github.com>
2024-12-12 01:10:55 +0200

    Time: 0 ms (100.00%) | Memory: 8.7 MB (23.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -29,3 +29,34 @@ public:
 
     }
 };
+
+
+/////////////////////////////// ANOTHER SOLUTION ///////////////////////////
+//class Solution {
+//public:
+//    bool isValid(string s) {
+//        stack<char>stk;
+//
+//
+//        for(i=0 ; i<s.length() ; i++) {
+//            if (c == '(' || c == '[' || c == '{') {
+//                stack.push(c);
+//            } else {
+//                // If the stack is empty, there is no matching opening bracket, so return false
+//                if (stack.isEmpty()) {
+//                    return false;
+//                }
+//                char top = stack.peek();
+//                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+//                    // If it is, pop the opening bracket from the stack
+//                    stack.pop();
+//                } else { // Otherwise, the brackets don't match, so return false
+//                    return false;
+//                }
+//            }
+//
+//        }
+//        return stack.isEmpty();
+//        
+//    }
+//};
