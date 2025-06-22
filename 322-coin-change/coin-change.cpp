ab896a6685641eb49d1e3419cb7b97543f423d91
NadaAshrafZayed <118112548+NadaAshrafZayed@users.noreply.github.com>
2025-06-22 07:16:42 +0300

    Time: 21 ms (74.54%) | Memory: 17.9 MB (83.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // This is a classic “bottom-up” dynamic programming (DP) solution.
+        // This is the “bottom-up” dp approach.
 
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
 
         for (int i = 1; i <= amount; i++) {
             for (int j = 0; j < coins.size(); j++) {
-                if (i - coins[j] >= 0) {
-                    dp[i] = min( dp[i] , dp[i - coins[j]] + 1);
+                if (coins[j] - i <= 0) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
                 }
             }
         }
 
-        int ans = dp[amount]; // store the result
-        if (ans == amount + 1) {
-            return -1;           // not possible
-        } else {
-            return ans;       // return the computed value
+        if(dp[amount] > amount){
+            return -1;
         }
-
-
+        return dp[amount];
     }
 };
+
+
+/*
+// This is the “top-down” dp approach.
+
+class Solution {
+public:
+    unordered_map<int, int> memo;
+    int dfs(int amount, vector<int>& coins) {
+        if (amount == 0) return 0;
+        if (memo.find(amount) != memo.end()) 
+            return memo[amount];
+
+        int res = INT_MAX;
+        for (int coin : coins) {
+            if (amount - coin >= 0) {
+                int result = dfs(amount - coin, coins);
+                if (result != INT_MAX) {
+                    res = min(res, 1 + result);
+                }
+            }
+        }
+        
+        memo[amount] = res;
+        return res;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int minCoins = dfs(amount, coins);
+        return minCoins == INT_MAX ? -1 : minCoins;
+    }
+};
+
+*/
