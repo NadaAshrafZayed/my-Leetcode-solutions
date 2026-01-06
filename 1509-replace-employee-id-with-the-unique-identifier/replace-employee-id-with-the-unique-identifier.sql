457c2def755d4a2ec315f9c9548600f7c2866ab3
NadaAshrafZayed <118112548+NadaAshrafZayed@users.noreply.github.com>
2026-01-06 23:43:21 +0200

    Time: 1211 ms (28.36%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees
-LEFT JOIN EmployeeUNI
-ON
-(Employees.id = EmployeeUNI.id);
-
-
-
---  If a user does not have a unique ID replace just show null.
--- 3shan kda I used el LEFTTTT JOIN
+SELECT EmployeeUNI.unique_id, Employees.name  
+FROM Employees LEFT JOIN EmployeeUNI
+ON Employees.id = EmployeeUNI.id;
